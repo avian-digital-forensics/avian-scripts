37987555aa6a97ea868f060a1bcb1bad53d40ce1
Albert Garde <aga@avian.dk>
2019-07-04 17:30:36 +0200

    Add wssCaller script

---
+++ wssCaller.rb
@@ -0,0 +1,3 @@
+# A simple script that calls other WSSs
+scriptPath = "C:/Users/Albert/Documents/scripts/avian-scripts/wss/email-address-fixer/emailAddressFixerWSS.rb" # The absolute path to the worker side script.
+load scriptPath
