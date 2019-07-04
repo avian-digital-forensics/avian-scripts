0589e8edfeb4cc96f4b35df1a710c763c082a579
Albert Garde <aga@avian.dk>
2019-07-04 19:28:06 +0200

    Change default file path

--- wssCaller.rb
+++ wssCaller.rb
@@ -1,3 +1,3 @@
 # A simple script that calls other WSSs
-scriptPath = "C:/Users/Albert/Documents/scripts/avian-scripts/wss/email-address-fixer/emailAddressFixerWSS.rb" # The absolute path to the worker side script.
+scriptPath = "REPLACE WITH PATH TO SCRIPT" # The absolute path to the worker side script.
 load scriptPath
