d2178a1a69a30793606806c4b9e0a7254d5dd32a
Albert Garde <aga@avian.dk>
2019-07-07 16:10:14 +0200

    Allow paths with backslashes

--- wss_caller.rb
+++ wss_caller.rb
@@ -1,13 +1,15 @@
 def nuix_worker_item_callback(worker_item)
-    path = "REPLACE WITH PATH TO DIRECTORY" # EDIT: The absolute path to the avian scripts directory.
+    # EDIT: The absolute path to the avian scripts directory.
+    path = 'REPLACE WITH PATH TO DIRECTORY'
 
     scripts = [ # EDIT: List of all worker side scripts that should be executed, in order.
-    "example script one", # This list is not case-sensitive,
-    "example_script_two", # and all spaces and underscores are ignored.
-    "eXample_scRiPtTHREe" # You can add as many as you like, just remember to end all lines but the last with a comma.
+    'example script one', # This list is not case-sensitive,
+    'example_script_two', # and all spaces and underscores are ignored.
+    'eXample_scRiPtTHREe' # You can add as many as you like, just remember to end all lines but the last with a comma.
     ]
     
     # Actually runs the scripts. Do not touch.
-    load (path + "/wss_dispatcher")
+    path.gsub!('\\', '/')
+    load (path + '/wss_dispatcher.rb')
     dispatch_scripts(path, scripts, worker_item)
 end
