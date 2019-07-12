d28eec9497745be2b461d41be6ba56a4f4ace4e2
Albert Garde <aga@avian.dk>
2019-07-12 18:26:25 +0200

    Add function to find selected radio button

--- utils/nx_utils.rb
+++ utils/nx_utils.rb
@@ -1,6 +1,8 @@
 script_directory = File.dirname(__FILE__)
+# Finds the path to the Nx.jar in the resources directory.
 require File.join(script_directory,"..","resources","Nx.jar")
 
+# Imports a mess of nx graphics modules.
 java_import "com.nuix.nx.NuixConnection"
 java_import "com.nuix.nx.LookAndFeelHelper"
 java_import "com.nuix.nx.dialogs.ChoiceDialog"
@@ -11,16 +13,25 @@ java_import "com.nuix.nx.dialogs.ProcessingStatusDialog"
 java_import "com.nuix.nx.digest.DigestHelper"
 java_import "com.nuix.nx.controls.models.Choice"
 
+# Changes to windows look and feel.
 LookAndFeelHelper.setWindowsIfMetal
 
+# This is probably necessary.
 NuixConnection.setUtilities($utilities)
 
+# This too.
 NuixConnection.setCurrentNuixVersion(NUIX_VERSION)
 
 module NXUtils
     extend self
     
+    # Simply creates a dialog with the given title.
     def create_dialog(title)
         return TabbedCustomDialog.new(title)
+    end
+    
+    def radio_group_value(dialog_values, radio_options)
+        radio_keys = radio_options.values # Yes, this does make sense.
+        return radio_keys.select{ |key| dialog_values[key] }.first
     end
 end
