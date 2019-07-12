1e16ee9cd4e9197868c2b156b2dca10db3d0a4f1
Albert Garde <aga@avian.dk>
2019-07-12 13:20:06 +0200

    Finds all addresses sent to by single address

--- scripts/connected-addresses/connected_addresses.rb
+++ scripts/connected-addresses/connected_addresses.rb
@@ -2,6 +2,109 @@
 # Needs Case: true
 require 'set'
 
+
+address = "Stabs Chef"
+
+output_destination_path = 'C:\Users\Albert\Documents'
+output_filename = 'test'
+output_file_extension = '.csv'
+
+delimiter = ','
+
+class Recipient
+    
+    def initialize(address)
+        @address = address
+        @tos = 0
+        @ccs = 0
+        @bccs = 0
+    end
+    
+    def add_to
+        @tos += 1
+    end
+    
+    def add_cc
+        @ccs += 1
+    end
+    
+    def add_bcc
+        @bccs += 1
+    end
+    
+    def tos
+        @tos
+    end
+    
+    def ccs
+        @ccs
+    end
+    
+    def bccs
+        @bccs
+    end
+    
+    def total
+        return @tos + @ccs + @bccs
+    end
+    
+    def to_s(delimiter)
+        return @address + delimiter + @tos.to_s + delimiter + @ccs.to_s + delimiter + @bccs.to_s + delimiter + total.to_s
+    end
+end
+
+class ToConnections
+    
+    def initialize()
+        @recipients = {}
+    end
+    
+    def add_to(address)
+        add_if_missing(address)
+        @recipients[address].add_to
+    end
+    
+    def add_cc(address)
+        add_if_missing(address)
+        @recipients[address].add_cc
+    end
+    
+    def add_bcc(address)
+        add_if_missing(address)
+        @recipients[address].add_bcc
+    end
+    
+    def add_communication_item(item)
+        communication = item.getCommunication()
+        tos = communication.getTo().map{ |to| to.getAddress() }
+        ccs = communication.getCc().map{ |cc| cc.getAddress() }
+        bccs = communication.getBcc().map{ |bcc| bcc.getAddress() }
+        for address in tos
+            add_to(address)
+        end
+        for address in ccs
+            add_cc(address)
+        end
+        for address in bccs
+            add_bcc(address)
+        end
+        return
+    end
+    
+    def to_s(delimiter)
+        @recipients.values.reduce(""){ |total, recipient| total + recipient.to_s(delimiter) + "\n" }
+    end
+    
+    private
+        def add_if_missing(address)
+            if !@recipients.key?(address)
+                @recipients[address] = Recipient.new(address)
+            end
+        end
+end
+         
+        
+
 def recipient_addresses(email)
     communication = email.getCommunication()
     tos = communication.getTo().map{ |to| to.getAddress() }
@@ -19,8 +122,15 @@ def all_from_addresses(email)
     return communication.getFrom().map{ |from| from.getAddress() }
 end
 
-address = "Stabs Chef"
 emails_from = currentCase.search("from:\"" + address + "\" has-communication:1")
-recipient_addresses = emails_from.reduce(Set[]){ |total, email| total.merge(all_recipient_addresses(email))}
+to_connections = ToConnections.new
+for item in emails_from
+    to_connections.add_communication_item(item)
+end
 
-mails_to = currentCase.search("from:\"" + address + "\" has-communication:1")
+file_path = File.join(output_destination_path, output_filename + output_file_extension)
+file = File.open(file_path, 'w')
+file.puts('address' + delimiter + 'to' + delimiter + 'cc' + delimiter + 'bcc' + delimiter + 'total')
+file.puts(to_connections.to_s(delimiter))
+file.close
+puts(to_connections.to_s(delimiter))
