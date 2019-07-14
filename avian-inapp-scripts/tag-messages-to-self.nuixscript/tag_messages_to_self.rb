b4bceb56e46c40383508e909cb2c8e4c6d7a5bc4
Albert Garde <aga@avian.dk>
2019-07-14 12:56:17 +0200

    Add messages about script progress

--- avian-inapp-scripts/tag-messages-to-self.nuixscript/tag_messages_to_self.rb
+++ avian-inapp-scripts/tag-messages-to-self.nuixscript/tag_messages_to_self.rb
@@ -1,20 +1,35 @@
+puts("Running script...")
+
 items = currentCase.search("has-communication:1")
+
+puts("Communications found: " + items.length.to_s)
 
 base_tag = "SentToSender"
 to_suffix = "To"
 cc_suffix = "Cc"
 bcc_suffix = "Bcc"
 
+numTo = 0
+numCc = 0
+numBcc = 0
+
 for item in items.each
     communication = item.communication
-    from = communication.from
+    from = communication.from[0]
     if communication.to.any?{ |to| to.address == from.address }
         item.addTag(base_tag + to_suffix)
+        numTo += 1
     end
     if communication.cc.any?{ |cc| cc.address == from.address }
         item.addTag(base_tag + cc_suffix)
+        numCc += 1
     end
     if communication.bcc.any?{ |bcc| bcc.address == from.address }
         item.addTag(base_tag + bcc_suffix)
+        numBcc += 1
     end
 end
+
+puts("Found " + numTo.to_s + " items with from in to.")
+puts("Found " + numCc.to_s + " items with from in cc.")
+puts("Found " + numBcc.to_s + " items with from in bcc.")
