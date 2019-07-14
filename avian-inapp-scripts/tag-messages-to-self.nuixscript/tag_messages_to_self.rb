b059e3693eee0fa828685858bb6deac6e284c9e3
Albert Garde <aga@avian.dk>
2019-07-14 12:39:12 +0200

    First draft of script

--- avian-inapp-scripts/tag-messages-to-self.nuixscript/tag_messages_to_self.rb
+++ avian-inapp-scripts/tag-messages-to-self.nuixscript/tag_messages_to_self.rb
@@ -1,1 +1,20 @@
 items = currentCase.search("has-communication:1")
+
+base_tag = "SentToSender"
+to_suffix = "To"
+cc_suffix = "Cc"
+bcc_suffix = "Bcc"
+
+for item in items.each
+    communication = item.communication
+    from = communication.from
+    if communication.to.any?{ |to| to.address == from.address }
+        item.addTag(base_tag + to_suffix)
+    end
+    if communication.cc.any?{ |cc| cc.address == from.address }
+        item.addTag(base_tag + cc_suffix)
+    end
+    if communication.bcc.any?{ |bcc| bcc.address == from.address }
+        item.addTag(base_tag + bcc_suffix)
+    end
+end
