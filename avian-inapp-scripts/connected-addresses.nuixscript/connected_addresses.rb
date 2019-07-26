1f98c6bf0e9b16cad9f07d37f67edcd9823c041e
Albert Garde <aga@avian.dk>
2019-07-26 21:25:11 +0200

    Removed unnecessary duplicate files

--- avian-inapp-scripts/connected-addresses.nuixscript/connected_addresses.rb
+++ avian-inapp-scripts/connected-addresses.nuixscript/connected_addresses.rb
@@ -1,5 +1,14 @@
 script_directory = File.dirname(__FILE__)
-require File.join(script_directory,"utils","nx_utils")
+require File.join(script_directory,"..","setup.nuixscript","get_main_directory")
+
+main_directory = get_main_directory(false)
+
+if not main_directory
+    puts("Script cancelled.")
+    return
+end
+
+require File.join(main_directory,"utils","nx_utils")
 
 
 dialog = NXUtils.create_dialog("Connected Addresses")
