3b5ef6a000046876650e08dbde91e2d987f37294
Albert Garde <aga@avian.dk>
2019-07-26 12:16:37 +0200

    Read and set settings

---
+++ avian-inapp-scripts/wss-setup.nuixscript/wss_setup.rb
@@ -0,0 +1,57 @@
+require 'yaml'
+require 'fileutils'
+script_directory = File.dirname(__FILE__)
+require File.join(script_directory,"utils","nx_utils")
+
+main_directory = ""
+while main_directory == ""
+    input = CommonDialogs.get_directory("", "Main Script Directory").path
+    if input.empty?
+        CommonDialogs.show_warning("You must specify the path to the main script directory.")
+        next false
+    elsif not File.directory?(input)
+        CommonDialogs.show_warning("The specified main script directory path is not a directory.")
+        next false
+    elsif not File.file?(input + "/wss_caller.rb")
+        CommonDialogs.show_warning("Wrong path specified for main script directory. The directory required is the one with the file 'wss_caller.rb'.")
+        next false
+    else
+        main_directory = input
+    end
+end
+
+settings_file = File.join(main_directory, 'data', 'wss_settings.yml')
+
+# If the settings file does not exist, create it from defaults.
+if not File.file?(settings_file)
+    FileUtils.cp(File.join(main_directory, 'data', 'default_wss_settings.yml'), settings_file)
+end
+
+wss_settings = YAML.load(File.read(settings_file))
+
+dialog = TabbedCustomDialog.new("Connected Addresses")
+
+# Add main tab.
+main_tab = dialog.add_tab("main_tab", "Main")
+
+for script in wss_settings[:scripts]
+    main_tab.append_check_box(script[:identifier], script[:label], script[:active])
+end
+
+dialog.validate_before_closing do |values|
+    
+    # Everything is fine; close the dialog.
+    next true
+end
+
+dialog.display
+
+if dialog.get_dialog_result == true
+    puts("Applying settings...")
+    values = dialog.to_map
+    for script in wss_settings[:scripts]
+        script[:active] = values[script[:identifier]]
+    end
+    File.open(settings_file, "w") { |file| file.write(wss_settings.to_yaml) }
+    puts("Scripts finished.")
+end
