e827c5865452e87ab986ca9b68d8eaede05de361
Albert Garde <aga@avian.dk>
2019-08-23 10:40:16 +0200

    Setup script files for exclude-duplicate-emails

---
+++ avian-inapp-scripts/exclude-duplicate-emails.nuixscript/exclude_duplicate_emails.rb
@@ -0,0 +1,42 @@
+script_directory = File.dirname(__FILE__)
+require File.join(script_directory,"..","setup.nuixscript","get_main_directory")
+
+main_directory = get_main_directory(false)
+
+if not main_directory
+    puts("Script cancelled.")
+    return
+end
+
+require File.join(main_directory,"utils","nx_utils")
+
+
+dialog = NXUtils.create_dialog("Template")
+
+# Add main tab.
+main_tab = dialog.addTab("main_tab", "Main")
+
+# TODO: ADD GUI HERE
+
+
+# Checks the input before closing the dialog.
+dialog.validateBeforeClosing do |values|
+    
+    # TODO: ADD CHECKS HERE
+    
+    # Everything is fine; close the dialog.
+    next true
+end
+
+# Display dialog. Duh.
+dialog.display
+
+# If dialog result is false, the user has cancelled.
+if dialog.getDialogResult == true
+    puts("Running script...")
+    
+    # values contains the information the user inputted.
+    values = dialog.toMap
+    
+    # TODO: Add script here.
+end
