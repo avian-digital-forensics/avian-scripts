d3528477d7eae64245344e285c51d030a2aed054
Albert Garde <aga@avian.dk>
2019-09-03 12:40:01 +0200

    Add CaseInformation class

--- utils/settings_utils.rb
+++ utils/settings_utils.rb
@@ -1,7 +1,40 @@
+require 'yaml'
 require 'fileutils'
 
 module SettingsUtils
     extend self
+    
+    # Holds information about a nuix case that can be saved to settings.
+    class CaseInformation
+        attr_accessor :guid, :name, :data_path
+        
+        class << self
+            private :new
+        end
+        
+        # Creates a new CaseInformation about the given case.
+        def self.store_case_information(nuix_case, main_directory)
+            case_information = new
+            case_information.guid = nuix_case.guid
+            case_information.name = nuix_case.name
+            case_information.data_path = SettingsUtils::case_data_dir(main_directory, nuix_case)
+            return case_information
+        end
+        
+        # Creates a hash meant to be stored in yaml, that contains all the necessary information to recreate the case.
+        def to_yaml_hash
+            return {:guid => @guid, :name => @name, :data_path =>  @data_path}
+        end
+        
+        # Loads case information from a hash.
+        def self.from_yaml_hash(yaml_hash)
+            case_information = new
+            case_information.guid = yaml_hash[:guid]
+            case_information.name = yaml_hash[:name]
+            case_information.data_path = yaml_hash[:data_path]
+            return case_information
+        end
+    end
 
     # Gets the data directory for the specified case.
     def case_data_dir(main_directory, current_case)
@@ -12,7 +45,7 @@ module SettingsUtils
         
         # If the directory already exists, check if it is the same case.
         if File.directory?(dir_name)
-            dir_guid = File.read(File.join(dir_name, "guid.txt"))
+            dir_guid = File.read(File.join(dir_name, "guid.txt")).strip
             # If not, use a different directory.
             if dir_guid != case_guid
                 dir_name = File.join(main_directory, "data", "cases", case_guid)
