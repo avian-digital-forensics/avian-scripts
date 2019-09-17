df19cfe9f01cfac2a2144b3c92bf3043cdd245e8
Albert Garde <aga@avian.dk>
2019-09-17 11:38:54 +0200

    Add timings

--- avian-inapp-scripts/tag-exchange-addresses-with-duplicates.nuixscript/tag_exchange_addresses_with_duplicates.rb
+++ avian-inapp-scripts/tag-exchange-addresses-with-duplicates.nuixscript/tag_exchange_addresses_with_duplicates.rb
@@ -57,61 +57,73 @@ if dialog.getDialogResult == true
     puts("Running script...")
     
     timer = Timer::Timer.new
+    
     timer.start("total")
-    
-    # values contains the information the user inputted.
-    values = dialog.toMap
-    
-    # If item text starts with this, the item is from store A.
-    store_a_prefix = values["store_a_prefix"]
-    
-    # All exchange server emails will receive this tag.
-    store_a_tag = values["store_a_tag"]
-    
-    # All exchange server emails with an archived duplicate will receive this tag.
-    has_archived_duplicate_metadata_name = values["has_archived_duplicate_metadata_name"]
-    
-    # Returns the ID of the specified email.
-    def find_email_id(email)
-        raise ArgumentError, "Email doesn't have a Mapi-Smtp-Message-Id property. GUID: " + email.guid unless email.properties.key?("Mapi-Smtp-Message-Id")
-        return email.properties["Mapi-Smtp-Message-Id"]
-    end
-    
-    puts("Finding exchange server emails...")
-    timer.start("find_store_a")
-    # Tag all exchange server emails.
-    store_a_items = current_case.search('kind:email AND content:"' + store_a_prefix + '"')
-    store_a_size = store_a_items.length
-    one_percent = (store_a_size/100).floor
-    puts(one_percent.to_s)
-    cur_count = 0
-    for email in store_a_items
-        email.add_tag(store_a_tag)
-        cur_count += 1
-        if cur_count % one_percent == 0
-            percent = cur_count / one_percent
-            if percent % 5 == 0
-                puts(percent.to_s + "%")
+    runs = 0
+    while timer.total_time("total") < 60
+        runs += 1
+        # values contains the information the user inputted.
+        values = dialog.toMap
+        
+        # If item text starts with this, the item is from store A.
+        store_a_prefix = values["store_a_prefix"]
+        
+        # All exchange server emails will receive this tag.
+        store_a_tag = values["store_a_tag"]
+        
+        # All exchange server emails with an archived duplicate will receive this tag.
+        has_archived_duplicate_metadata_name = values["has_archived_duplicate_metadata_name"]
+        
+        # Returns the ID of the specified email.
+        def find_email_id(email)
+            raise ArgumentError, "Email doesn't have a Mapi-Smtp-Message-Id property. GUID: " + email.guid unless email.properties.key?("Mapi-Smtp-Message-Id")
+            return email.properties["Mapi-Smtp-Message-Id"]
+        end
+        
+        puts("Finding exchange server emails...")
+        timer.start("find_store_a")
+        # Tag all exchange server emails.
+        store_a_items = current_case.search('kind:email AND content:"' + store_a_prefix + '"')
+        store_a_size = store_a_items.length
+        one_percent = (store_a_size/100).floor
+        puts(one_percent.to_s)
+        cur_count = 0
+        for email in store_a_items
+            email.add_tag(store_a_tag)
+            cur_count += 1
+            if cur_count % one_percent == 0
+                percent = cur_count / one_percent
+                if percent % 5 == 0
+                    puts(percent.to_s + "%")
+                end
             end
         end
-    end
-    timer.stop("find_store_a")
-    
-    # All ID's used by archived emails.
-    timer.start
-    archive_id_set = Set.new(current_case.search('kind:email AND NOT tag:' + store_a_prefix)){ |archived_email| find_email_id(archived_email) }
-    
-    num_without_duplicate = 0
-    
-    # Give all exchange server emails custom metadata for whether there is an archived duplicate.
-    for email in current_case.search('tag:' + store_a_prefix)
-        if archive_id_set.include?(find_email_id(email))
-            email.custom_metadata[has_archived_duplicate_metadata_name] = TRUE
-        else
-            email.custom_metadata[has_archived_duplicate_metadata_name] = FALSE
-            num_without_duplicate += 1
+        timer.stop("find_store_a")
+        
+        timer.start("non_store_a_search")
+        # All ID's used by archived emails.
+        archive_id_set = Set.new(current_case.search('kind:email AND NOT tag:' + store_a_prefix)){ |archived_email| find_email_id(archived_email) }
+        timer.stop("non_store_a_search")
+        
+        num_without_duplicate = 0
+        
+        timer.start("has_duplicate")
+        # Give all exchange server emails custom metadata for whether there is an archived duplicate.
+        for email in current_case.search('tag:' + store_a_prefix)
+            if archive_id_set.include?(find_email_id(email))
+                email.custom_metadata[has_archived_duplicate_metadata_name] = TRUE
+            else
+                email.custom_metadata[has_archived_duplicate_metadata_name] = FALSE
+                num_without_duplicate += 1
+            end
         end
+        timer.stop("has_duplicate")
     end
+    puts("Timings:")
+    puts("    total: " + Timer::seconds_to_string(timer.total_time("total")/runs))
+    puts("    find_store_a: " + Timer::seconds_to_string(timer.total_time("find_store_a")/runs))
+    puts("    non_store_a_search: " + Timer::seconds_to_string(timer.total_time("non_store_a_search")/runs))
+    puts("    has_duplicate: " + Timer::seconds_to_string(timer.total_time("has_duplicate")/runs))
     
     # Tell the user if emails without archived duplicates were found.
     if num_without_duplicate > 0
@@ -119,6 +131,7 @@ if dialog.getDialogResult == true
         puts("They have been a custom metadata field '" + has_archived_duplicate_metadata_name + "' with value FALSE.")
         CommonDialogs.show_information("A total of " + num_without_duplicate.to_s + " exchange server emails without an archived duplicate were found.")
     end
+    
     timer.stop("total")
     
     # Tell the user the script has finished.
