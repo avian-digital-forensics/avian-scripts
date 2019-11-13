c0c73e7f6c9b86a6e106ecff892aedd8c6d43981
Albert Garde <aga@avian.dk>
2019-11-13 11:16:31 +0100

    Add individual batch timings

--- avian-inapp-scripts/batched-ocr.nuixscript/batched_ocr.rb
+++ avian-inapp-scripts/batched-ocr.nuixscript/batched_ocr.rb
@@ -13,6 +13,8 @@ end
 
 # Progress messages.
 require File.join(main_directory,"utils","utils")
+# Timings.
+require File.join(main_directory,"utils","timer")
 # For GUI messages.
 require File.join(main_directory,"utils","nx_utils")
 java_import "com.nuix.nx.NuixConnection"
@@ -39,71 +41,91 @@ worker_settings_tab = dialog.addTab("worker_settings_tab","Worker Settings")
 worker_settings_tab.appendLocalWorkerSettings("worker_settings")
 
 dialog.validateBeforeClosing do |values|
-	if CommonDialogs.getConfirmation("The script needs to close all workbench tabs, proceed?") == false
-		next false
-	end
-	next true
+    if CommonDialogs.getConfirmation("The script needs to close all workbench tabs, proceed?") == false
+        next false
+    end
+    next true
 end
 
 dialog.display
 if dialog.getDialogResult == true
-	values = dialog.toMap
-	batch_index = 0
-	last_progress = Time.now
+    values = dialog.toMap
+    batch_index = 0
+    last_progress = Time.now
 
-	$window.closeAllTabs
+    $window.closeAllTabs
 
-	ProgressDialog.forBlock do |pd|
-		pd.setTitle("Batched OCR")
-		pd.logMessage("Selected Items: #{$current_selected_items.size}")
-		pd.logMessage("Batch Size: #{values["target_batch_size"]}")
+    ProgressDialog.forBlock do |pd|
+        pd.setTitle("Batched OCR")
+        pd.logMessage("Selected Items: #{$current_selected_items.size}")
+        pd.logMessage("Batch Size: #{values["target_batch_size"]}")
 
-		pd.logMessage("Worker Settings:")
-		values["worker_settings"].each do |k,v|
-			pd.logMessage("\t#{k} => #{v}")
-		end
+        pd.logMessage("Worker Settings:")
+        values["worker_settings"].each do |k,v|
+            pd.logMessage("\t#{k} => #{v}")
+        end
 
-		pd.logMessage("OCR Settings:")
-		values["ocr_settings"].each do |k,v|
-			pd.logMessage("\t#{k} => #{v}")
-		end
+        pd.logMessage("OCR Settings:")
+        values["ocr_settings"].each do |k,v|
+            pd.logMessage("\t#{k} => #{v}")
+        end
 
-		ocr_processor = $utilities.createOcrProcessor
-		ocr_processor.setParallelProcessingSettings(values["worker_settings"])
-		ocr_processor.whenItemEventOccurs do |info|
-			if (Time.now - last_progress) > 0.5
-				pd.setSubStatus("Stage: #{info.getStage.split("_").map{|v|v.capitalize}.join(" ")}")
-				last_progress = Time.now
-			end
-		end
+        ocr_processor = $utilities.createOcrProcessor
+        ocr_processor.setParallelProcessingSettings(values["worker_settings"])
+        ocr_processor.whenItemEventOccurs do |info|
+            if (Time.now - last_progress) > 0.5
+                pd.setSubStatus("Stage: #{info.getStage.split("_").map{|v|v.capitalize}.join(" ")}")
+                last_progress = Time.now
+            end
+        end
+        
+        timer = Timing::Timer.new
+        
+        total_batches = ($current_selected_items.size.to_f / values["target_batch_size"].to_f).ceil
+        pd.setMainProgress(0,total_batches)
+        stop_requested = false
+        $current_selected_items.each_slice(values["target_batch_size"]) do |slice_items|
+            break if pd.abortWasRequested
+            
+            # Start batch timer.
+            timer_name = "batch#{batch_index+1}"
+            if timer.exist?(timer_name)
+                puts("Unexpected behavior: several batches with same index.")
+                timer.reset(timer_name)
+            end
+            timer.start(timer_name)
+            
+            # Process batch.
+            pd.setSubProgress(0,slice_items.size)
+            pd.setMainStatusAndLogIt("Processing Batch #{batch_index+1}")
+            ocr_job = ocr_processor.processAsync(slice_items,values["ocr_settings"])
+            while !ocr_job.hasFinished
+                if pd.abortWasRequested && !stop_requested
+                    ocr_job.stop
+                    stop_requested = true
+                end
+                pd.setSubProgress(ocr_job.getCurrentStageExportedItemsCount,slice_items.size)
+                sleep(0.25)
+            end
+            
+            # Print timer result.
+            timer.stop(timer_name)
+            Utils.print_progress("Finished batch #{batch_index+1}")
+            pd.log_message("Finished batch #{batch_index+1}")
+            timing_message = "Time taken: " + timer.total_time(timer_name)
+            Utils.print_progress(timing_message)
+            pd.log_message(timing_message)
+            
+            batch_index += 1
+            pd.setMainProgress(batch_index)
+        end
 
-		total_batches = ($current_selected_items.size.to_f / values["target_batch_size"].to_f).ceil
-		pd.setMainProgress(0,total_batches)
-		stop_requested = false
-		$current_selected_items.each_slice(values["target_batch_size"]) do |slice_items|
-			break if pd.abortWasRequested
-			pd.setSubProgress(0,slice_items.size)
-			pd.setMainStatusAndLogIt("Processing Batch #{batch_index+1}")
-			ocr_job = ocr_processor.processAsync(slice_items,values["ocr_settings"])
-			while !ocr_job.hasFinished
-				if pd.abortWasRequested && !stop_requested
-					ocr_job.stop
-					stop_requested = true
-				end
-				pd.setSubProgress(ocr_job.getCurrentStageExportedItemsCount,slice_items.size)
-				sleep(0.25)
-			end
-            Utils.print_progress("Finished batch " + batch_index)
-			batch_index += 1
-			pd.setMainProgress(batch_index)
-		end
+        if pd.abortWasRequested
+            pd.setMainStatusAndLogIt("User Aborted")
+        else
+            pd.setCompleted
+        end
 
-		if pd.abortWasRequested
-			pd.setMainStatusAndLogIt("User Aborted")
-		else
-			pd.setCompleted
-		end
-
-		$window.openTab("workbench",{"search"=>""})
-	end
+        $window.openTab("workbench",{"search"=>""})
+    end
 end
