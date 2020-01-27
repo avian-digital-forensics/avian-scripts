837bdccd198086e0aa83d344724ab7b27ddb436c
Albert Garde <aga@avian.dk>
2020-01-27 17:49:12 +0100

    Add exclusion of items

--- avian-inapp-scripts/qc-cull.nuixscript/main.rb
+++ avian-inapp-scripts/qc-cull.nuixscript/main.rb
@@ -35,6 +35,14 @@ main_tab = dialog.add_tab('main_tab', 'Main')
 main_tab.append_text_field('num_descendants_metadata_key', 'Number of descendants custom metadata name', script_settings[:main][:num_descendants_metadata_key])
 main_tab.get_control('num_descendants_metadata_key').set_tool_tip_text('All items will receive a custom metadata field with this key.')
 
+# Add text field for exclude tag prefix.
+main_tab.append_text_field('exclude_tag_prefix', 'Exclusion tag prefix', script_settings[:main][:exclude_tag_prefix])
+main_tab.get_control('exclude_tag_prefix').set_tool_tip_text('All items with a tag starting with this will be excluded. If left blank, no items will be excluded.')
+
+# Add text field for exclude reason.
+main_tab.append_text_field('exclude_reason', 'Exclusion reason', script_settings[:main][:exclude_reason])
+main_tab.get_control('exclude_reason').set_tool_tip_text('All exclusions will be given this reason.')
+
 
 # Add search and tag file tab.
 search_and_tag_tab = dialog.add_tab('search_and_tag', 'Search and Tag')
@@ -46,6 +54,7 @@ while script_settings[:search_and_tag].key?("search_and_tag_file_#{search_and_ta
     search_and_tag_tab.get_control("search_and_tag_file_#{search_and_tag_file_num}").set_tool_tip_text('This file will be loaded into the search and tag.')
     search_and_tag_tab.set_text("search_and_tag_file_#{search_and_tag_file_num}", script_settings[:search_and_tag]["search_and_tag_file_#{search_and_tag_file_num}".to_sym])
 end
+
 
 
 # Checks the input before closing the dialog.
@@ -69,9 +78,14 @@ if dialog.dialog_result
     # values contains the information the user inputted.
     values = dialog.to_map
 
-    script_settings[:main][:num_descendants_metadata_key] = values['num_descendants_metadata_key']
-
     num_descendants_metadata_key = values['num_descendants_metadata_key']
+    script_settings[:main][:num_descendants_metadata_key] = num_descendants_metadata_key
+
+    exclude_tag_prefix = values['exclude_tag_prefix']
+    script_settings[:main][:exclude_tag_prefix] = exclude_tag_prefix
+
+    exclude_reason= values['exclude_reason']
+    script_settings[:main][:exclude_reason] = exclude_reason
 
     search_and_tag_files = []
     for k in 1..search_and_tag_file_num
@@ -102,7 +116,7 @@ if dialog.dialog_result
         
         progress_dialog.set_main_status_and_log_it('Adding selected item tag...')
         timer.start('add_selected_items_tag')
-        Utils::bulk_add_tag(utilities, progress_dialog, timer, selected_item_tag, items)
+        Utils::bulk_add_tag(utilities, progress_dialog, selected_item_tag, items)
         timer.stop('add_selected_items_tag')
 
         if search_and_tag_files.empty?
@@ -111,11 +125,38 @@ if dialog.dialog_result
             QCCull::search_and_tag(current_case,progress_dialog,timer,search_and_tag_files,'tag:"' + selected_item_tag + '"')
         end
 
+        if exclude_tag_prefix.empty?
+            progress_dialog.log_message('Skipping exclude because exclude tag prefix is blank.')
+        else
+            timer.start('exclude_items')
+
+            progress_dialog.set_main_status_and_log_it('Finding exclusion tags...')
+            timer.start('find_exclude_tags')
+            exclude_tags = current_case.all_tags.select { |tag| tag.start_with?(exclude_tag_prefix) }
+            timer.stop('find_exclude_tags')
+
+            progress_dialog.set_main_status_and_log_it('Finding exclusion items...')
+            timer.start('find_exclude_items')
+            exclude_search = exclude_tags.map { |tag| "tag:\"#{tag}\""}.join(' OR ')
+            if exclude_search.empty?
+                progress_dialog.log_message('Skipping exclusion since no matching tags were found.')
+                timer.stop('find_exclude_items')
+            else
+                exclude_search += " AND tag:\"#{selected_item_tag}\""
+                exclude_items = current_case.search(exclude_search)
+                timer.stop('find_exclude_items')
+    
+                progress_dialog.set_main_status_and_log_it('Excluding items...')
+                Utils::bulk_exclude(utilities, progress_dialog, exclude_items, exclude_reason)
+            end
+
+            timer.stop('exclude_items')
+        end
 
         
         progress_dialog.set_main_status_and_log_it('Removing selected item tag...')
         timer.start('remove_selected_items_tag')
-        Utils::bulk_remove_tag(utilities, progress_dialog, timer, selected_item_tag, items)
+        Utils::bulk_remove_tag(utilities, progress_dialog, selected_item_tag, items)
         timer.stop('remove_selected_items_tag')
 
         if current_case.delete_tag(selected_item_tag)
