a26f0172d4ae2ee477e1b978f919ce9381ea76e6
Albert Garde <aga@avian.dk>
2020-01-27 13:14:06 +0100

    Make number_of_descendants calleable from other scripts

--- avian-inapp-scripts/number-of-descendants.nuixscript/number_of_descendants.rb
+++ avian-inapp-scripts/number-of-descendants.nuixscript/number_of_descendants.rb
@@ -1,69 +1,26 @@
-# Standard code for finding main directory.
-script_directory = File.dirname(__FILE__)
-require File.join(script_directory,"..","setup.nuixscript","get_main_directory")
-
-main_directory = get_main_directory(false)
-
-unless main_directory
-    puts("Script cancelled.")
-    return
-end
-
-# For GUI.
-require File.join(main_directory,"utils","nx_utils")
-
-gui_title = "Number of Descendants"
-
-dialog = NXUtils.create_dialog(gui_title)
-
-# Add main tab.
-main_tab = dialog.addTab("main_tab", "Main")
-
-# Add a text field for the custom metadata name.
-main_tab.appendTextField("metadata_key", "Custom metadata name", "NumberOfDescendants")
-main_tab.getControl("metadata_key").setToolTipText("The metadata field items receive will have this name.")
-
-
-# Checks the input before closing the dialog.
-dialog.validateBeforeClosing do |values|
-    
-    unless NXUtils.assert_non_empty_field(values, "metadata_key", "custom metadata name")
-        next false
-    end
-    
-    # Everything is fine; close the dialog.
-    next true
-end
-
-# Display dialog. Duh.
-dialog.display
-
-# If dialog result is false, the user has cancelled.
-if dialog.getDialogResult == true
-    puts("Running script...")
-    
-    # values contains the information the user inputted.
-    values = dialog.toMap
-    
-    metadata_key = values["metadata_key"]
+module NumberOfDescendants
+    extend self
     
     # Returns the number of descendants of the item.
     def num_descendants(item)
         return item.descendants.length
     end
-    
-    items = current_selected_items
-    puts("Processing " + current_selected_items.length.to_s + " items...")
-    
-    # Add a custom metadata field to each item with the number of descendants.
-    for item in items
-        item.custom_metadata[metadata_key] = num_descendants(item)
+
+    def number_of_descendants(current_case, progress_dialog, timer, items, metadata_key)
+
+        progress_dialog.set_sub_progress_visible(false)
+        
+        # Add metadata to items.
+        progress_dialog.set_main_status_and_log_it('Finding number of descendants of items...')
+        timer.start('find_num_descendants')
+        progress_dialog.set_main_progress(0, items.size)
+        items.each_with_index do |item, item_index|
+            # Add a custom metadata field to each item with the number of descendants.
+            item.custom_metadata[metadata_key] = num_descendants(item)
+
+            progress_dialog.increment_main_progress
+            progress_dialog.set_sub_status("#{item_index+1}/#{items.size}")
+        end
+        timer.stop('find_num_descendants')
     end
-    
-    # Tell the user the script has finished.
-    CommonDialogs.show_information("Script finished." , gui_title)
-    
-    puts("Script finished.")
-else
-    puts("Script cancelled.")
 end
