80922e8627276c91a4e2a4c6dca2cb9fca4cc8fa
Albert Garde <aga@avian.dk>
2020-01-29 16:02:34 +0100

    Add comments to QCCull.search_and_tag

--- avian-inapp-scripts/qc-cull.nuixscript/search_and_tag.rb
+++ avian-inapp-scripts/qc-cull.nuixscript/search_and_tag.rb
@@ -1,14 +1,17 @@
 module QCCull
     extend self
 
+    # Performs Search and Tag in the given case.
+    # Progress is shown in the main progress bar of the given progress dialog.
     def search_and_tag(current_case, progress_dialog, timer, search_and_tag_files, scoping_query)
+        # Initialize a bulk searcher with the given scoping query.
         bulk_searcher = current_case.create_bulk_searcher.with_scoping_query(scoping_query)
+        # Imports the specified files to the bulk searcher.
         for file in search_and_tag_files
             bulk_searcher.import_file(file)
         end
 
         progress_dialog.set_sub_progress_visible(false)
-        
         
         progress_dialog.set_main_status_and_log_it('Performing search and tag...')
         timer.start('search_and_tag')
