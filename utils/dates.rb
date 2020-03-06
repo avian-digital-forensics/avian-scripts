89fbea25cb298c451e71a6193bdf36f4b90fb1fb
Albert Garde <aga@avian.dk>
2020-03-06 11:12:02 +0100

    Add method to convert ruby DateTime to Joda DateTime

--- utils/dates.rb
+++ utils/dates.rb
@@ -1,3 +1,11 @@
+# If this package is used, ruby DateTime will be renamed RubyDateTime to free up the namespace for Joda DateTime.
+
+require 'date'
+RubyDateTime = DateTime
+DateTime = nil
+java_import 'org.joda.time.DateTime'
+java_import 'org.joda.time.DateTimeZone'
+
 module Dates
     def danish_date_string_to_english(date_string)
         if date_string.count('.') == 1 # If there are more than this, the date probably uses . as a seperator.
@@ -16,5 +24,16 @@ module Dates
             date_string.gsub!('key', 'value')
         end
         return date_string
+    end
+
+    def self.date_time_to_joda_time(date_time)
+        date = date_time.to_date
+        offset_hours = (date_time.offset*24).floor
+        puts('torsk: ' + offset_hours.to_s)
+        offset_minutes = (date_time.offset.to_f*24 - offset_hours)*60
+        puts('sej: ' + offset_minutes.to_s)
+        offset = DateTimeZone.for_offset_hours_minutes(offset_hours, offset_minutes)
+
+        DateTime.new(date.year, date.month, date.day, date_time.hour, date_time.minute, date_time.second, offset)
     end
 end
