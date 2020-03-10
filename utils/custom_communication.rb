c416d050f2fa078cd39fb5fd2985c11b8afe3dca
Albert Garde <aga@avian.dk>
2020-03-10 16:48:14 +0100

    Switch to YAML from CSV

--- utils/custom_communication.rb
+++ utils/custom_communication.rb
@@ -8,12 +8,11 @@ module Custom
         
         attr_accessor :personal, :address
 
-		def initialize(address)
-			@personal = address.personal
-			@address = address.address
+		def self.from_address(address)
+			return CustomAddress.new(address.personal, address.address)
 		end
 
-		def self.create_custom(personal, address)
+		def initialize(personal, address)
 			@personal = personal
 			@address = address
 		end
@@ -45,30 +44,45 @@ module Custom
 		def equals(address)
 			address == @address
 		end
+
+		def to_yaml_hash
+			return { :personal => @personal, :address => @address }
+		end
+
+		def self.from_yaml_hash(yaml_hash)
+			return CustomAddress.new(yaml_hash[:personal], yaml_hash[:address])
+		end
 	end
 
 	class CustomCommunication
 		include Communication
-        attr_accessor :date_time, :subject, :from_addresses, :to_addresses, :cc_addresses, :bcc_addresses
+        attr_accessor :joda_time, :subject, :from_addresses, :to_addresses, :cc_addresses, :bcc_addresses
 
-		def initialize(communication)
-			@date_time = communication.date_time
-			@subject = ''
-			@from_addresses = if communication.from then communication.from else [] end
-			@to_addresses = if communication.to then communication.to else [] end
-			@cc_addresses = if communication.cc then communication.cc else [] end
-			@bcc_addresses = if communication.bcc then communication.bcc else [] end
+		def self.from_communication(communication)
+			joda_time = communication.date_time
+			subject = ''
+			from_addresses = if communication.from then communication.from else [] end
+			to_addresses = if communication.to then communication.to else [] end
+			cc_addresses = if communication.cc then communication.cc else [] end
+			bcc_addresses = if communication.bcc then communication.bcc else [] end
+			return CustomCommunication.new(joda_time, subject, from_addresses, to_addresses, cc_addresses, bcc_addresses)
 		end
 
 		# joda_time should always be a Joda DateTime
-		def self.create_custom(joda_time, subject, from_addresses, to_addresses, cc_addresses, bcc_addresses)
-			unless joda_time.is_a?(DateTime) raise ArgumentError 'joda_time must be a Joda DateTime.' end
+		def initialize(joda_time, subject, from_addresses, to_addresses, cc_addresses, bcc_addresses)
+			unless joda_time.is_a?(DateTime) 
+				raise ArgumentError 'joda_time must be a Joda DateTime.'
+			end
 			@joda_time = joda_time
 			@subject = subject
-			@from_addresses = from_addresses
-			@to_addresses = to_addresses
-			@cc_addresses = cc_addresses
-			@bcc_addresses = bcc_addresses
+			@from_addresses = from_addresses.map { |address| CustomAddress.from_address(address) }
+			@to_addresses = to_addresses.map { |address| CustomAddress.from_address(address) }
+			@cc_addresses = cc_addresses.map { |address| CustomAddress.from_address(address) }
+			@bcc_addresses = bcc_addresses.map { |address| CustomAddress.from_address(address) }
+		end
+
+		def date_time
+			@joda_time
 		end
 
 		def getDateTime
@@ -100,20 +114,30 @@ module Custom
             @bcc_addresses = bcc_addresses
 		end
 		
-		def to_csv_array()
-			return Dates::joda_time_to_csv_array(@joda_time) ++ [@subject, @from, @to, @cc, @bcc]
+		def to_yaml_hash()
+			yaml_hash = {}
+			yaml_hash[:joda_time] = @joda_time.to_s
+			yaml_hash[:subject] = @subject
+			yaml_hash[:from] = @from_addresses.map(&:to_yaml_hash)
+			yaml_hash[:to] = @to_addresses.map(&:to_yaml_hash)
+			yaml_hash[:cc] = @cc_addresses.map(&:to_yaml_hash)
+			yaml_hash[:bcc] = @bcc_addresses.map(&:to_yaml_hash)
+			return yaml_hash
 		end
 
-		def self.from_csv_array(csv_array)
-			joda_time_array = csv_array[0, Dates::joda_time_csv_array_length]
-			csv_array = csv_array[Dates::joda_time_csv_array_length..-1]
-
-			@joda_time = Dates::joda_time_from_csv_array(joda_time_array)
-			@subject = array[0]
-			@from = array[1]
-			@to = array[2]
-			@cc = array[3]
-			@bcc = array[4]
+		def self.from_yaml_hash(yaml_hash)
+			joda_time = DateTime.parse(yaml_hash[:joda_time])
+			subject = yaml_hash[:subject]
+			from_addresses = address_list_from_yaml_hash_list(yaml_hash[:from])
+			to_addresses = address_list_from_yaml_hash_list(yaml_hash[:to])
+			cc_addresses = address_list_from_yaml_hash_list(yaml_hash[:cc])
+			bcc_addresses = address_list_from_yaml_hash_list(yaml_hash[:bcc])
+			return CustomCommunication.new(joda_time, subject, from_addresses, to_addresses, cc_addresses, bcc_addresses)
 		end
+
+		private
+			def self.address_list_from_yaml_hash_list(yaml_hash_list)
+				return yaml_hash_list.map { |address_yaml_hash| CustomAddress.from_yaml_hash(address_yaml_hash) }
+			end
     end
 end
--- utils/dates.rb
+++ utils/dates.rb
@@ -7,6 +7,8 @@ java_import 'org.joda.time.DateTime'
 java_import 'org.joda.time.DateTimeZone'
 
 module Dates
+    extend self
+
     def danish_date_string_to_english(date_string)
         if date_string.count('.') == 1 # If there are more than this, the date probably uses . as a seperator.
             date_string.delete!('.') # For danish dates using the format 4. november.
@@ -28,8 +30,8 @@ module Dates
 
     # Converts an offset in days to a Joda DateTimeZone.
     def offset_to_date_time_zone(offset)
-        offset_hours = (date_time.offset*24).floor
-        offset_minutes = (date_time.offset*24 - offset_hours)*60
+        offset_hours = (offset*24).floor
+        offset_minutes = (offset*24 - offset_hours)*60
         DateTimeZone.for_offset_hours_minutes(offset_hours, offset_minutes)
     end
 
@@ -42,39 +44,5 @@ module Dates
 
     def joda_time_csv_array_length
         7
-    end
-
-    # Converts a Joda DateTime to an array that can be saved in csv.
-    def joda_time_to_csv_array(joda_time)
-        year = joda_time.year
-        month = joda_time.month_of_year
-        day = joda_time.day_of_month
-        hour = joda_time.hour_of_day
-        minute = minute_of_hour
-        second = second_of_minute
-        millisecond_offset = joda_time.get_standard_offset(0)
-
-        csv_array = [year, month, day, hour, minute, second, millisecond_offset]
-        unless csv_array.size == joda_time_csv_array_length 
-            raise 'Sum ting wong'
-        end
-        return csv_array
-    end
-
-    # Creates a Joda DateTime from an array.
-    def joda_time_from_csv_array(csv_array)
-        unless csv_array.size == joda_time_csv_array_length 
-            raise ArgumentError, 'The array must have exactly seven elements.'
-        end
-        year = csv_array[0].to_i
-        month = csv_array[1].to_i
-        day = csv_array[2].to_i
-        hour = csv_array[3].to_i
-        minute = csv_array[4].to_i
-        second = csv_array[5].to_i
-        offset = csv_array[6].to_i
-        time_zone = DateTimeZone.for_offset_millis(offset)
-
-        DateTime.new(year, month, day, hour, minute, second, time_zone)
     end
 end
