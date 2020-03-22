bd797b62b5324f6a41a5b8dd41a795c6c3e50e9c
Albert Garde <aga@avian.dk>
2020-03-22 20:14:30 +0100

    First draft of WSS

---
+++ wss/add-communication-to-unidentified-emails/add_communication_to_unidentified_emails.rb
@@ -0,0 +1,40 @@
+module AddCommunicationToUnidentifiedEmails
+    extend self
+    
+    def run_init(wss_global)
+        # Will be run once before loading items.
+        # Setup script here.
+        require 'yaml'
+
+        root_path = wss_global.root_path
+
+        require File.join(root_path, 'utils', 'custom_communication')
+
+        # Finds the data file path.
+        data_path = File.join(wss_global.case_data_path, 'unidentified_emails_data.yml')
+        if File.file?(data_path)
+            # Load data from file and save to wss_global.
+            data = YAML.load_file(data_path)
+            wss_global.vars[:add_communication_to_unidentified_emails_data] = data
+            wss_global.vars[:add_communication_to_unidentified_emails_has_data] = true
+        else
+            wss_global.vars[:add_communication_to_unidentified_emails_has_data] = false
+            STDERR.puts("Could not find data file. Did you remember to run 'FixUnidentifiedEmails'?")
+        end
+    end
+    
+    def run(wss_global, worker_item)
+        # Will be run for each item.
+        # This is the main body of the script.
+        if wss_global.vars[:add_communication_to_unidentified_emails_has_data]
+            data = wss_global.vars[:add_communication_to_unidentified_emails_data]
+            if data.key?(worker_item.item_guid)
+                # Create a CustomCommunication from the guid's data.
+                communication = Custom::CustomCommunication::from_yaml_hash(data[worker_item.item_guid])
+                worker_item.set_item_communication(communication)
+            end
+        else
+            STDERR.puts("AddCommunicationToUnidentifiedEmails: No data file. Skipping.")
+        end
+    end
+end
