42e4e6c94066baad0e323d87a49a5d95d483d044
Albert Garde <aga@avian.dk>
2020-03-29 18:27:36 +0200

    Don't import JodaTime

--- utils/dates.rb
+++ utils/dates.rb
@@ -1,16 +1,16 @@
 # If this package is used, ruby DateTime will be renamed RubyDateTime to free up the namespace for Joda DateTime.
 
 require 'date'
-RubyDateTime = DateTime
-DateTime = nil
-java_import 'org.joda.time.DateTime'
-java_import 'org.joda.time.DateTimeZone'
+JodaTime = org.joda.time
+#RubyDateTime = Object.send(:remove_const, :DateTime)
+#java_import 'org.joda.time.DateTime'
+#java_import 'org.joda.time.DateTimeZone'
 
 module Dates
     extend self
 
     def danish_date_string_to_english(date_string)
-        if date_string.count('.') == 1 # If there are more than this, the date probably uses . as a seperator.
+        if date_string[0,4].count('.') == 1 && date_string.count('.') == 1 # If there are more than this, the date probably uses . as a seperator.
             date_string.delete!('.') # For danish dates using the format 4. november.
         end
         translations = {
@@ -23,7 +23,7 @@ module Dates
             'oktober' => 'October'
         }
         for key,value in translations
-            date_string.gsub!('key', 'value')
+            date_string.gsub!(key, value)
         end
         return date_string
     end
@@ -32,14 +32,14 @@ module Dates
     def offset_to_date_time_zone(offset)
         offset_hours = (offset*24).floor
         offset_minutes = (offset*24 - offset_hours)*60
-        DateTimeZone.for_offset_hours_minutes(offset_hours, offset_minutes)
+        JodaTime::DateTimeZone.for_offset_hours_minutes(offset_hours, offset_minutes)
     end
 
     def date_time_to_joda_time(date_time)
         date = date_time.to_date
         time_zone = offset_to_date_time_zone(date_time.offset)
-
-        DateTime.new(date.year, date.month, date.day, date_time.hour, date_time.minute, date_time.second, time_zone)
+		
+        return JodaTime::DateTime.new(date.year, date.month, date.day, date_time.hour, date_time.minute, date_time.second, time_zone)
     end
 
     def joda_time_csv_array_length
