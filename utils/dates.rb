ec2d90c1a4212309aaf4c52c5848ad1d2f25fdca
Albert Garde <aga@avian.dk>
2020-03-31 10:03:13 +0200

    Add doc

--- utils/dates.rb
+++ utils/dates.rb
@@ -9,10 +9,12 @@ JodaTime = org.joda.time
 module Dates
     extend self
 
+    # Takes a date string that may be either Danish or English and translates it to English if it is Danish.
     def danish_date_string_to_english(date_string)
         if date_string[0,4].count('.') == 1 && date_string.count('.') == 1 # If there are more than this, the date probably uses . as a seperator.
             date_string.delete!('.') # For danish dates using the format 4. november.
         end
+        # Most of these are unnecessary since ruby's Date class can figure it out by itself. However, oktober doesn't work so that must be done explicitly.
         translations = {
             'januar' => 'January',
             'februar' => 'February',
@@ -35,14 +37,11 @@ module Dates
         JodaTime::DateTimeZone.for_offset_hours_minutes(offset_hours, offset_minutes)
     end
 
+    # Converts a Ruby DateTime to a JodaTime DateTime.
     def date_time_to_joda_time(date_time)
         date = date_time.to_date
         time_zone = offset_to_date_time_zone(date_time.offset)
 		
         return JodaTime::DateTime.new(date.year, date.month, date.day, date_time.hour, date_time.minute, date_time.second, time_zone)
-    end
-
-    def joda_time_csv_array_length
-        7
     end
 end
