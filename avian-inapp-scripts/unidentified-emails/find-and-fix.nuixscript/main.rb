e977a7df808dfba86a4c911d459e3dd314c6f9b5
Albert Garde <aga@avian.dk>
2020-04-03 09:51:40 +0200

    Add missing arguments

---
+++ avian-inapp-scripts/unidentified-emails/find-and-fix.nuixscript/main.rb
@@ -0,0 +1,167 @@
+script_directory = File.dirname(__FILE__)
+require File.join(script_directory,'..','..','setup.nuixscript','get_main_directory')
+
+main_directory = get_main_directory(false)
+
+unless main_directory
+    puts('Script cancelled because no main directory could be found.')
+    return
+end
+
+# For GUI.
+require File.join(main_directory,'utils','nx_utils')
+# Timings.
+require File.join(main_directory,'utils','timer')
+# Progress messages.
+require File.join(main_directory,'utils','utils')
+
+require File.join(main_directory,'utils','utils')
+
+require File.join(main_directory,'avian-inapp-scripts','unidentified-emails','find-unidentified-emails.nuixscript','find_unidentified_emails')
+
+require File.join(main_directory,'avian-inapp-scripts','unidentified-emails','fix-unidentified-emails.nuixscript','fix_unidentified_emails')
+
+
+gui_title = 'Find and Fix Unidentified Emails'
+
+dialog = NXUtils.create_dialog(gui_title)
+
+# Add main tab.
+main_tab = dialog.add_tab('main_tab', 'Main')
+
+main_tab.append_check_box('process_unselected_rfc_mails', 'Process unselected RFC mails', true)
+main_tab.get_control('process_unselected_rfc_mails').set_tool_tip_text('Whether to process all RFC mail items in the case or only those in the selection.')
+
+main_tab.append_text_field('allowed_start_offset', 'Allowed start offset', '10')
+main_tab.get_control('allowed_start_offset').set_tool_tip_text('Number of non-whitespace characters allowed before "from".')
+
+main_tab.append_text_field('start_area_size', 'Start area size', '400')
+main_tab.get_control('start_area_size').set_tool_tip_text('Size of the area from the start of the items content in which the email information must appear.')
+
+main_tab.append_text_field('email_tag', 'Email tag', 'UnidentifiedEmail')
+main_tab.get_control('email_tag').set_tool_tip_text('The tag given to all found emails.')
+
+
+# Checks the input before closing the dialog.
+dialog.validate_before_closing do |values|
+    
+    # Make sure allowed start offset is not empty.
+    if values['allowed_start_offset'].strip.empty?
+        CommonDialogs.show_warning('Please provide an allowed start offset.', gui_title)
+        next false
+    else 
+        allowed_start_offset = Integer(values['allowed_start_offset'].strip) rescue false
+        if not allowed_start_offset or allowed_start_offset < 0
+            CommonDialogs.show_warning('Allowed start offset must be a positive integer.', gui_title)
+            next false
+        end
+    end
+
+    # Make sure start area size is not empty.
+    if values['start_area_size'].strip.empty?
+        CommonDialogs.show_warning('Please provide a start area size.', gui_title)
+        next false
+    else 
+        start_area_size = Integer(values['start_area_size'].strip) rescue false
+        if not start_area_size or start_area_size < 0
+            CommonDialogs.show_warning('Start area size must be a positive integer.', gui_title)
+            next false
+        end
+    end
+
+    if values['email_tag'].strip.empty?
+        CommonDialogs.show_warning('Please provide an email tag.', gui_title)
+        next false
+    end
+
+    # Everything is fine; close the dialog.
+    next true
+end
+
+# Display dialog. Duh.
+dialog.display
+
+# If dialog result is false, the user has cancelled.
+if dialog.dialog_result
+    Utils.print_progress('Running script...')
+    
+    # values contains the information the user inputted.
+    values = dialog.to_map
+
+    timer = Timing::Timer.new
+    timer.start('total')
+
+    process_unselected_rfc_mails = values['process_unselected_rfc_mails']
+    allowed_start_offset = Integer(values['allowed_start_offset'])
+    start_area_size = Integer(values['start_area_size'])
+    email_tag = values['email_tag']
+    
+    ProgressDialog.for_block do |progress_dialog|
+        # Setup progress dialog.
+        progress_dialog.set_title(gui_title)
+        progress_dialog.on_message_logged do |message|
+            Utils.print_progress(message)
+        end
+        progress_dialog.set_sub_progress_visible(false)
+        
+        bulk_annotater = utilities.get_bulk_annotater
+        
+        # Find which items to run on.
+        progress_dialog.set_main_status_and_log_it('Making preliminary search...')
+        if current_selected_items.size > 0 
+            # If items are selected, run on those.
+            progress_dialog.log_message('Using selection. Skipping preliminary search.')
+            items = current_selected_items
+        else
+            # If not, do a specific search.
+            items = FindUnidentifiedEmails::preliminary_search(current_case, progress_dialog, timer)
+        end
+		
+        num_emails = FindUnidentifiedEmails::find_unidentified_emails(current_case, items, progress_dialog, timer, allowed_start_offset, start_area_size, email_tag, bulk_annotater)
+        
+        progress_dialog.log_message('Found ' + num_emails.to_s + ' emails.')
+        progress_dialog.set_main_status_and_log_it('Identifying which items to process...')
+
+        # Create set of items to be processed.
+        items = current_selected_items.to_set
+        if process_unselected_rfc_mails
+            items.merge(FixUnidentifiedEmails::find_rfc_mails(current_case))
+        end
+
+        # Find the case data directory.
+        case_data_dir = SettingsUtils::case_data_dir(main_directory, current_case)
+
+        communication_field_aliases = {
+            :date => ['date', 'Date', 'dato', 'Dato', 'sendt', 'Sendt', 'modtaget', 'Modtaget'],
+            :subject => ['subject', 'Subject', 'emne', 'Emne'],
+            :from => ['from', 'From', 'fra', 'Fra', 'afsender', 'Afsender'],
+            :to => ['to', 'To', 'til', 'Til', 'modtager', 'Modtager'],
+            :cc => ['cc', 'Cc'],
+            :bcc => ['bcc', 'Bcc']
+        }
+    
+        address_regexps = [
+            /(.*\b)\s*\[(.*)\]/,        # Addresses like Example Exampleson [example@ex.com]
+            /(.*\b)\s*\<(.*)\>/,        # Addresses like Example Exampleson <example@ex.com>
+            /\'?\"?()(.*@.*\b)\'?\"?/,  # Addresses like example@ex.com or 'example@ex.com'
+            /\'?\"?()(.*\b)\'?\"?/      # Addresses like Example Exampleson or 'Example Exampleson'
+        ]
+
+        progress_dialog.log_message('Found ' + items.size.to_s + ' items to process.')
+        progress_dialog.set_main_status_and_log_it('Finding communication fields for items...')
+        FixUnidentifiedEmails::fix_unidentified_emails(case_data_dir, current_case, items, progress_dialog, timer, communication_field_aliases, start_area_size, address_regexps) { |string| string.split(';').map(&:strip) }
+
+        timer.stop('total')
+        
+        timer.print_timings
+        
+        progress_dialog.set_main_status_and_log_it('Script finished.')
+
+        CommonDialogs.show_information('Script finished.', gui_title)
+        progress_dialog.set_completed
+    end
+    
+    Utils.print_progress('Script finished.')
+else
+    Utils.print_progress('Script cancelled.')
+end
