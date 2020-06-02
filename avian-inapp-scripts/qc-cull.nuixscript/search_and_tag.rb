4fdeece8c7f6543b7fcfb38a2ec269c6a42d3640
Albert Garde <aga@avian.dk>
2020-06-02 12:54:18 +0200

    :bug: Fix so scoping query works

--- avian-inapp-scripts/qc-cull.nuixscript/search_and_tag.rb
+++ avian-inapp-scripts/qc-cull.nuixscript/search_and_tag.rb
@@ -5,23 +5,24 @@ module QCCull
     # Progress is shown in the main progress bar of the given progress dialog.
     def search_and_tag(current_case, progress_dialog, timer, search_and_tag_files, scoping_query)
         # Initialize a bulk searcher with the given scoping query.
-        bulk_searcher = current_case.create_bulk_searcher.with_scoping_query(scoping_query)
+        bulk_searcher = current_case.create_bulk_searcher
         # Imports the specified files to the bulk searcher.
         for file in search_and_tag_files
             bulk_searcher.import_file(file)
         end
+        bulk_searcher = bulk_searcher.with_scoping_query(scoping_query)
 
         progress_dialog.set_sub_progress_visible(false)
         
         progress_dialog.set_main_status_and_log_it('Performing search and tag...')
         timer.start('search_and_tag')
-        num_items = current_case.search(bulk_searcher.scoping_query).size
-        item_num = 1
-        progress_dialog.set_main_progress(0, num_items)
+        num_rows = bulk_searcher.row_count
+        row_num = 0
+        progress_dialog.set_main_progress(0, num_rows)
         # Perform search
         bulk_searcher.run do |progress_info|
             progress_dialog.increment_main_progress
-            progress_dialog.set_sub_status("#{item_num += 1}/#{num_items}")
+            progress_dialog.set_sub_status("#{row_num += 1}/#{num_rows}")
         end
         timer.stop('search_and_tag')
     end
