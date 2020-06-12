0d24833ea3e2a1cc532ca591166c5121698ab084
Albert Garde <aga@avian.dk>
2020-06-12 14:20:28 +0200

    :art: Modify inapp_gui_template to cpmly with new tab size

--- utils/templates/inapp_gui_template.rb
+++ utils/templates/inapp_gui_template.rb
@@ -5,8 +5,8 @@ require File.join(setup_directory,'inapp_script')
 # GUI_TITLE is the name given to all GUI elements created by the InAppScript.
 # SCRIPT_NAME should be of the form inapp_gui_template2.
 unless script = Script::create_inapp_script(setup_directory, GUI_TITLE, SCRIPT_NAME)
-    STDERR.puts('Could not find main directory.')
-    return
+  STDERR.puts('Could not find main directory.')
+  return
 end
 
 # Main directory path can be found in script.main_directory.
@@ -19,10 +19,10 @@ end
 script.dialog_add_tab('main_tab', 'Main')
 
 script.dialog_append_check_box('main_tab', 'example_checkbox', 'Example Checkbox', 
-        'Description of checkbox.')
+    'Description of checkbox.')
 
 script.dialog_append_text_field('main_tab', 'example_text_field', 'Example text field',
-        'Description of text field.')
+    'Description of text field.')
 
 options = { 'Button 1' => 'button_1', 'Button 2' => 'button_2' }
 script.dialog_append_horizontal_radio_button_group('main_tab', 'example_horizontal_radio_button', 'Example horizontal radio button', options)
@@ -32,19 +32,19 @@ script.dialog_append_vertical_radio_button_group('main_tab', 'example_vertical_r
 
 # Checks the input before closing the dialog.
 script.dialog_validate_before_closing do |values|
-    
-    # TODO: ADD CHECKS HERE
-    
-    # Everything is fine; close the dialog.
-    next true
+  
+  # TODO: ADD CHECKS HERE
+  
+  # Everything is fine; close the dialog.
+  next true
 end
 
 # Display dialog. Receive input from user. Run script.
 script.run do |progress_dialog|
 
-    timer = script.timer
-    
-    # TODO: ADD SCRIPT HERE.
-    
-    return 'RESULT_MESSAGE'
+  timer = script.timer
+  
+  # TODO: ADD SCRIPT HERE.
+  
+  return 'RESULT_MESSAGE'
 end
