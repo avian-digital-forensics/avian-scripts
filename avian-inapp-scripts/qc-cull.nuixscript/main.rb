41c4e66c112463d200f4c07334c461db1692d3e9
Albert Garde <aga@avian.dk>
2020-06-12 11:22:47 +0200

    :art: Move exclusion logic to its own method and file

--- avian-inapp-scripts/qc-cull.nuixscript/main.rb
+++ avian-inapp-scripts/qc-cull.nuixscript/main.rb
@@ -148,37 +148,7 @@ script.run do |progress_dialog|
   else
     timer.start('exclude_items')
     
-    progress_dialog.set_main_status_and_log_it("Excluding items...")
-    exclude_tag_prefixes.each do |prefix, reason|
-      # Create a list of which tags in the case are exclusion tags.
-      # All items with these tags will be excluded.
-      progress_dialog.set_main_status_and_log_it("Finding exclusion tags with prefix '#{prefix}'...")
-      timer.start('find_exclude_tags')
-      exclude_tags = current_case.all_tags.select { |tag| tag.start_with?(prefix) }
-      timer.stop('find_exclude_tags')
-
-
-      # Finds all selected items with exclusion tags.
-      progress_dialog.set_main_status_and_log_it('Finding exclusion items...')
-      timer.start('find_exclude_items')
-      # Create a search string matching all items with exclusion tags.
-      exclude_search = exclude_tags.map { |tag| "tag:\"#{tag}\""}.join(' OR ')
-      if exclude_search.empty?
-        # If there are no exclusion tags, skip exclusion.
-        progress_dialog.log_message("Skipping exclusion for prefix '#{prefix}' since no matching tags were found.")
-        timer.stop('find_exclude_items')
-      else
-        # Add a clause to ensure that only selected items will match the search.
-        exclude_search += " AND tag:\"#{selected_item_tag}\""
-        # Perform the search.
-        exclude_items = current_case.search(exclude_search)
-        timer.stop('find_exclude_items')
-  
-        # Actually exclude the items.
-        progress_dialog.set_main_status_and_log_it("Excluding items for prefix '#{prefix}'...")
-        Utils::bulk_exclude(utilities, progress_dialog, exclude_items, reason)
-      end
-    end
+    QCCull::exclude_items("tag:\"#{selected_item_tag}\"", exclude_tag_prefixes, progress_dialog, timer, utilities)
     timer.stop('exclude_items')
   end
 
