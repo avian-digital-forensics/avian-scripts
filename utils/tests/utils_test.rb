89b22a7522882fdeb70ec177ad6b154ca1060203
Albert Garde <aga@avian.dk>
2020-08-25 15:25:11 +0200

    :white_check_mark: Add tests for add_hyphens_to_guid

--- utils/tests/utils_test.rb
+++ utils/tests/utils_test.rb
@@ -21,4 +21,17 @@ class TestUtils < Test::Unit::TestCase
         array1 = [1,2,3,4]
         array2 = [5,6,7,8]
         assert(Utils::sets_disjoint?(array1, array2))
+    end
+
+    def test_hyphenate_guid_do_nothing
+        pre_guid = '023423b7-1568-4b1f-bbcb-aa809850c3f1'
+        post_guid = Utils::add_hyphens_to_guid(pre_guid)
+        assert(pre_guid == post_guid)
+    end
+
+    def test_hyphenate_guid_add_hyphens
+        pre_guid = '023423b715684b1fbbcbaa809850c3f1'
+        post_guid = Utils::add_hyphens_to_guid(pre_guid)
+        assert(post_guid == '023423b7-1568-4b1f-bbcb-aa809850c3f1')
+    end
 end
