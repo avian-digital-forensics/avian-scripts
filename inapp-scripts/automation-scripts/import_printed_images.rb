6d7a3621f875693c3006bb2b6693495db0c56f9d
Albert Garde <aga@avian.dk>
2020-10-19 16:37:40 +0200

    :bug: Make the scoping query setting actually optional

--- inapp-scripts/automation-scripts/import_printed_images.rb
+++ inapp-scripts/automation-scripts/import_printed_images.rb
@@ -8,8 +8,10 @@ module ImportPrintedImages
 
         timer = Timing::Timer.new
         timer.start('total')
-
-        items = nuix_case.search(settings_hash[:scoping_query])
+        
+        # If a scoping query is given, use that.
+        scoping_query = settings_hash.key?(:scoping_query) ? settings_hash[:scoping_query] : ''
+        items = nuix_case.search(scoping_query)
         source_path = settings_hash[:source_path]
         ImportPrintedImages::import_printed_images(items, source_path, progress_handler, timer, utilities)
 
