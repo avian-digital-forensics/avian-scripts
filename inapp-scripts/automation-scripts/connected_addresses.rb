69811bde04a93f0dc0aef927726327f65b23b8ad
Albert Garde <aga@avian.dk>
2020-11-02 11:49:17 +0100

    :sparkles: Add ConnectedAddresses automation script

---
+++ inapp-scripts/automation-scripts/connected_addresses.rb
@@ -0,0 +1,27 @@
+module ConnectedAddresses
+    extend self
+
+    def run(nuix_case, utilities, settings_hash, progress_handler)
+        main_directory = settings_hash[:main_directory]
+        require File.join(main_directory,'utils','timer')
+        require File.join(main_directory,'inapp-scripts', 'connected-addresses', 'connected_addresses')
+
+        timer = Timing::Timer.new
+        timer.start('total')
+
+        # The address whose recipients are wanted.
+        address = settings_hash['primary_address']
+      
+        # The output path.
+        file_path = settings_hash['output_path']
+      
+        # The delimiter used in the CSV.
+        delimiter = settings_hash['delimiter']
+        
+        ConnectedAddresses::connected_addresses(nuix_case, progress_handler, timer, file_path, delimiter)
+
+        progress_handler.log_message("Script finished.")
+    
+        timer.stop('total')
+    end
+end
