f7f4d6840cf26fe1af1db673c0427a4fb224258b
Albert Garde <aga@avian.dk>
2020-11-04 11:41:12 +0100

    :bug: Fix typo

--- avian-inapp-scripts/export-printed-images.nuixscript/main.rb
+++ avian-inapp-scripts/export-printed-images.nuixscript/main.rb
@@ -49,7 +49,7 @@ script.run do |progress_dialog|
     scoping_query += " AND tag:\"#{selected_item_tag}\""
   end
 
-  images_exported = ExportPrintedImages::export_printed_images(script.main_directory, progress_dialog, timer, utilities, current_case, scoping_query)''
+  images_exported = ExportPrintedImages::export_printed_images(script.main_directory, progress_dialog, timer, utilities, current_case, scoping_query)
 
   next "Exported a total of #{images_exported.to_s} printed images."
 end
