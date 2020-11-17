b185e4ac4ce6b85ba70a25f84083ca1a9c39a537
Albert Garde <aga@avian.dk>
2020-11-17 13:05:25 +0100

    :art: Switch to 2 space tabs

--- inapp-scripts/automation-scripts/export_items.rb
+++ inapp-scripts/automation-scripts/export_items.rb
@@ -1,21 +1,21 @@
 module ExportItems
-    extend self
+  extend self
 
-    def run(nuix_case, utilities, settings_hash, progress_handler)
-        main_directory = settings_hash[:main_directory]
-        require File.join(main_directory,'utils','timer')
-        require File.join(main_directory,'inapp-scripts', 'export-items', 'export_items')
+  def run(nuix_case, utilities, settings_hash, progress_handler)
+    main_directory = settings_hash[:main_directory]
+    require File.join(main_directory,'utils','timer')
+    require File.join(main_directory,'inapp-scripts', 'export-items', 'export_items')
 
-        timer = Timing::Timer.new
-        timer.start('total')
-      
-        # If a scoping query is given, use that.
-        scoping_query = settings_hash.key?(:scoping_query) ? settings_hash[:scoping_query] : ''
-        items = nuix_case.search(scoping_query)
-        
-        script_finished_message = ExportItems::export_items(current_case, progress_dialog, timer, utilities, items, settings_hash)
+    timer = Timing::Timer.new
+    timer.start('total')
+    
+    # If a scoping query is given, use that.
+    scoping_query = settings_hash.key?(:scoping_query) ? settings_hash[:scoping_query] : ''
+    items = nuix_case.search(scoping_query)
+    
+    script_finished_message = ExportItems::export_items(current_case, progress_dialog, timer, utilities, items, settings_hash)
 
-        progress_handler.log_message("Script finished. #{script_finished_message}")
-        timer.stop('total')
-    end
+    progress_handler.log_message("Script finished. #{script_finished_message}")
+    timer.stop('total')
+  end
 end
