368c1d4c5012b95a03de54ef04e80f17c1d108e9
Albert Garde <albertsgarde@gmail.com>
2021-01-05 12:28:54 +0100

    :sparkles: Create new script

---
+++ _root/wss/load-call-record-as-communication/load_call_record_as_communication.rb
@@ -0,0 +1,51 @@
+require 'date'
+
+require_relative File.join('..','..','utils','custom_communication')
+require_relative File.join('..','..','utils','dates')
+
+module WSSTemplate
+    extend self
+
+
+    def run_init(wss_global)
+      # Will be run once before loading items.
+      # Setup script here.
+    end
+    
+    def run(wss_global, worker_item)
+        # TODO:
+        # The script could take some standard properties as being the various communication fields, and then a custom metadata profile could ensure that these were created.
+        source_item = worker_item.getSourceItem
+
+        if source_item.getType.getName == "application/x-database-table-row"
+          begin
+            properties = source_item.properties
+      
+            date_string = properties["Time"].to_s
+            date_time = DateTime.parse(date_string, '%Y-%m-%d %H:%M:%S')
+            joda_date_time = Dates::date_time_to_joda_time(date_time)
+
+            from_address_personal = properties["From name"]
+            from_address_address = properties["From no"]
+            from_address = Custom::CustomAddress::new(from_address_personal, from_address_address, "telephone-number")
+
+            to_address_personal = properties["To name"]
+            to_address_address = properties["To no"]
+            to_address = Custom::CustomAddress::new(to_address_personal, to_address_address, "telephone-number")
+
+            communication = Custom::CustomCommunication(joda_date_time, '', [from_address], [to_address])
+
+            worker_item.set_item_communication(communication)
+      
+          rescue Exception => ex
+            worker_item.addTag('Avian|WSS Error')
+            worker_item.addCustomMetadata('Processing Error',ex.inspect.to_s,'text','user')
+          end
+        end
+    end
+    
+    def run_close(wss_global)
+      # Will be run after loading all items.
+    end
+  end
+  
