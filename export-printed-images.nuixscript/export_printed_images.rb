349ba23a5828bdfa62c13a096e28f9e1c75c9037
Albert Garde <albertsgarde@gmail.com>
2021-01-08 14:53:24 +0100

    :bug: Fix syntax error

--- export-printed-images.nuixscript/export_printed_images.rb
+++ export-printed-images.nuixscript/export_printed_images.rb
@@ -1,10 +1,5 @@
 root_directory = File.expand_path('../../_root', __FILE__)
 
-unless main_directory
-    puts('Script cancelled because no main directory could be found.')
-    return
-end
-
 require File.join(main_directory, 'utils', 'utils')
 
 require File.join(main_directory, 'utils', 'settings_utils')
@@ -12,10 +7,10 @@ require File.join(main_directory, 'utils', 'settings_utils')
 module ExportPrintedImages
     extend self
 
-    def export_printed_images(main_directory, progress_handler, timer, utilities, nuix_case, scoping_query)
+    def export_printed_images(root_directory, progress_handler, timer, utilities, nuix_case, scoping_query)
         items = nuix_case.search(scoping_query)
 
-        directory = File.join(SettingsUtils::case_data_dir(main_directory, nuix_case.name, nuix_case.guid), 'printed_images')
+        directory = File.join(SettingsUtils::case_data_dir(root_directory, nuix_case.name, nuix_case.guid), 'printed_images')
         
         timer.start('export_images')
         images_exported = Utils::export_printed_images(items, directory, utilities, progress_handler)
